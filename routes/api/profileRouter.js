b066493d4226ac4039c6e74603a6a52521e497f0
JangMyungJoo <wkdaudwn11@naver.com>
2019-04-20 11:48:49 +0900

    add profileSchema And profile router

--- routes/api/profileRouter.js
+++ routes/api/profileRouter.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+const profileModel = require('../../models/profileModel');
+const userModel = require('../../models/userModel');
+
+const authCheck = passport.authenticate('jwt', {session: false});
 
 /**
  * @route   GET api/profile/test
@@ -7,5 +14,24 @@ const router = express.Router();
  * @access  Public
  */
 router.get('/test', (req, res) => res.json({msg: 'Profile Works'}));
+
+/**
+ * @route   GET api/profile
+ * @desc    Get current users profile
+ * @access  Private
+ */
+router.get('/', authCheck, (req, res) => {
+    const errors = {};
+
+    profileModel.findOne({user: req.user.id})
+        .then(profile => {
+            if(!profile){
+                errors.noprofile = 'There is no profile for this user';
+                return res.status(404).json(errors);
+            }
+            res.json(profile);
+        })
+        .catch(err => res.json(err));
+});
 
 module.exports = router;
