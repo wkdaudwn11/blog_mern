8e7774bad95b8506412d40ce0ad5d91d40b641d5
JangMyungJoo <wkdaudwn11@naver.com>
2019-04-20 09:38:36 +0900

    change model (avatar)

--- models/userModel.js
+++ models/userModel.js
@@ -15,6 +15,10 @@ const UserSchema = new Schema({
         type: String,
         required: true
     },
+    avatar: {
+        type: String,
+        required: true
+    },
     date: {
         type: String,
         default: Date.now
