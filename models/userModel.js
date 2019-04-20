5a3a9d8d9c8fbcf2e651a42de8c2ebd0b76a187e
JangMyungJoo <wkdaudwn11@naver.com>
2019-04-20 09:37:22 +0900

    add model (user)

---
+++ models/userModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+const UserSchema = new Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: String,
+        default: Date.now
+    }
+});
+
+const User = mongoose.model('users', UserSchema); // DB에 users를 생성하여 그 안에 UserSchema를 넣음
+module.exports = User;
+
+//module.exports = User = mongoose.model('users', UserSchema); <- 이렇게도 가능
