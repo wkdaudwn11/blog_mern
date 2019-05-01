398dd34fed5d7c639545a4497bfa9571619e7404
JangMyungJoo <wkdaudwn11@naver.com>
2019-05-01 12:16:51 +0900

    post add like, unlike routes

--- routes/api/postRouter.js
+++ routes/api/postRouter.js
@@ -87,4 +87,62 @@ router.delete("/:id", authCheck, (req,res) => {
     });
 });
 
+/**
+ * @route   Post api/post/like/:id
+ * @desc    Like post
+ * @access  private
+ */
+router.post("/like/:id", authCheck, (req,res) => {
+    profileModel.findOne({user: req.user.id})
+        .then(profile => {
+            postModel.findById(req.params.id)
+                .then(post => {
+                    if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0){
+                        return res.status(400).json({alreadyLiked: 'User already liked this post'});
+                    }
+
+                    // Add user id to likes array
+                    post.likes.unshift({user: req.user.id});
+                    post.save().then(post => res.json(post));
+                })
+                .catch(err => res.status(404).json(err));
+        })
+        .catch(err => res.status(404).json(err));
+});
+
+/**
+ * @route   Post api/post/unlike/:id
+ * @desc    Unlike post
+ * @access  private
+ */
+router.post("/unlike/:id", authCheck, (req,res) => {
+    profileModel.findOne({user: req.user.id})
+        .then(profile => {
+            postModel.findById(req.params.id)
+                .then(post => {
+                    if(post.likes.filter(like => like.user.toString() === req.user.id).length === 0){
+                        return res.status(400).json({notliked: 'You have not liked this post'});
+                    }
+
+                    // get remove index
+                    const removeIndex = post.likes
+                        .map(item => item.user.toString())
+                        .indexOf(req.user.id);
+
+                    // splice out of array
+                    post.likes.splice(removeIndex, 1);
+
+                    //save
+                    post.save()
+                        .then(post => {
+                            res.json(post);
+                            console.log(post);
+                        })
+                        .catch(err => res.status(404).json(err));
+                })
+                .catch(err => res.status(404).json(err));
+        })
+        .catch(err => res.status(404).json(err));
+});
+
 module.exports = router;
