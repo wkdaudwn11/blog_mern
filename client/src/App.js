e1de7db19a61edbc718aa30a1364c77bc2794701
JangMyungJoo <wkdaudwn11@naver.com>
2019-05-06 10:14:17 +0900

    Add navbar, landing and footer components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react'
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Footer from './components/layout/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>react</h1>
-    </div>
-  );
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <Landing />
+        <Footer />
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <footer className="bg-dark text-white mt-4 p-4 text-center test">
+            Copyright &copy; {new Date().getFullYear()} DevConnector
+        </footer>
+    );
+};
