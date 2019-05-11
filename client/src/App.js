90811d31f053844424791477ec9fc0922b5d7fb6
JangMyungJoo <wkdaudwn11@naver.com>
2019-05-11 10:10:53 +0900

    Redux store and Chrome ext setup
    client package.json에 proxy 설정 추가 (서버와 연결)

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react'
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+/** redux */
+import {Provider} from 'react-redux';
+import store from './store';
+
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
@@ -8,21 +13,22 @@ import Footer from './components/layout/Footer';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 
-
 export default class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route exact path="/" component={Landing} />
-          <div className="container">
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     )
   }
 }
---
+++ client/src/store.js
@@ -0,0 +1,20 @@
+/** Store.js는 로그인 정보, 프로필 등을 캐시에 담아주는 역할 */
+
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+    rootReducer,
+    initialState,
+    compose(
+        applyMiddleware(...middleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+);
+
+export default store;
