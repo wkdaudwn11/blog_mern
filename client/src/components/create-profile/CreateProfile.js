2539421991a2c1af193a83b101e38f1b5a73480b
JangMyungJoo <wkdaudwn11@naver.com>
2019-05-24 21:41:42 +0900

    Form field components

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import TextFieldGroup from '../common/TextFieldGroup';
+import InputGroup from '../common/InputGroup';
+import SelectListGroup from '../common/SelectListGroup';
 
 class CreateProfile extends Component{
 
@@ -53,4 +55,4 @@ const mapStateToProps = state => ({
     errors: state.errors
 });
 
-export default connect(null)(CreateProfile);
+export default connect(mapStateToProps)(CreateProfile);
