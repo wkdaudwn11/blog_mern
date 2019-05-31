88b538d7b2075cf999b9521a4c985153c12929d5
JangMyungJoo <wkdaudwn11@naver.com>
2019-05-31 21:25:42 +0900

    Add experience functionality

--- client/src/actions/profileAction.js
+++ client/src/actions/profileAction.js
@@ -73,3 +73,16 @@ export const deleteAccount = () => dispatch => {
             );
     }
 };
+
+// Add Experience
+export const addExperience = (expData, history) => dispatch => {
+    axios
+        .post('/api/profile/experience', expData)
+        .then(res => history.push('/dashboard'))
+        .catch(err => 
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        );
+};
